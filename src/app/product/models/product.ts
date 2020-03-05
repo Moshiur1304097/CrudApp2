d9a898205509c04b1f8a1843f18011fb7abee164
Moshiur Rahman <moshiurrahman6674@gmail.com>
2020-03-05 19:55:58 +0600

    start1

--- src/app/product/models/product.ts
+++ src/app/product/models/product.ts
@@ -1,2 +1,16 @@
-export class Product {
+import { Lookup } from './lookup';
+
+export interface IProduct{
+    id:number;
+    name: string;
+    code: string;
+    category: Lookup;
+    unit: Lookup;
+    salesRate: number;
+    purchaseRate: number;
 }
+
+export class Product {
+
+}
+
